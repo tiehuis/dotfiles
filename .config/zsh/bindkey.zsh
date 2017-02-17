ee1248338bd93210fa7ec62dc5d507885428d4df
tiehuis <marctiehuis@gmail.com>
2017-02-17 22:49:47 +1300

    [zsh] Add delete bindkey entry

--- .config/zsh/bindkey.zsh
+++ .config/zsh/bindkey.zsh
@@ -5,3 +5,4 @@ bindkey "$terminfo[kcuu1]" history-substring-search-up
 bindkey "$terminfo[kcud1]" history-substring-search-down
 bindkey "$terminfo[khome]" beginning-of-line
 bindkey "$terminfo[kend]"  end-of-line
+bindkey "$terminfo[kdch1]" delete-char

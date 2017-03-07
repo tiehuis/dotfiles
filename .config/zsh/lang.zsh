2b13e4c229852de83c44d4ca33dfc69d4de3140a
Marc Tiehuis <marctiehuis@gmail.com>
2017-03-07 21:52:01 +1300

    [ocaml] Remove configuration files

--- .config/zsh/lang.zsh
+++ .config/zsh/lang.zsh
@@ -9,7 +9,3 @@ export CARGO_HOME=~/.local/share/cargo
 
 # Ruby
 export PATH=$PATH:~/.gem/ruby/2.4.0/bin
-
-# Opam
-export OPAMROOT=~/.local/share/opam
-source ~/.local/share/opam/opam-init/init.zsh > /dev/null 2> /dev/null || true

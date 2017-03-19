44112638b7117650b228ccde81571b29f3b91a26
Marc Tiehuis <marctiehuis@gmail.com>
2017-03-19 20:35:28 +1300

    [zsh] Adjust config dirs + remove transmission/leafpad config
    Transmission/config files are not true config files and store too much
    information (such as window sizes) which make them not that useful to
    keep in a vcs.

--- .config/zsh/export.zsh
+++ .config/zsh/export.zsh
@@ -1,17 +1,22 @@
 export PATH=$PATH:~/bin
 export EDITOR='nvim'
-
-# Aliases for better .config conformance
-export LESSHISTFILE=-
+export VISUAL=$EDITOR
 
 export XDG_CACHE_HOME=~/.cache
 export XDG_CONFIG_HOME=~/.config
 export XDG_DATA_HOME=~/.local/share
 
 # configuration overrides to better match xdg specification
-export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/settings.ini
+export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/settings.ini"
 export WEECHAT_HOME="$XDG_CONFIG_HOME/weechat"
 export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
+export __GL_SHADER_DISK_CACHE_PATH="$XDG_CACHE_HOME/nv"
+export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
+
+# Aliases for better .config conformance
+export LESSHISTFILE=-
 
 # prevent wine creating menu/desktop entries
 export WINEDLLOVERRIDES="winemenubuilder.exe=d"

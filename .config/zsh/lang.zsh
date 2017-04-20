abf78fd1a88026bb6e8f2bf09231845ccf1d561e
Marc Tiehuis <marctiehuis@gmail.com>
2017-04-20 23:14:19 +1200

    [zsh] Make rustup conform to XDG spec

--- .config/zsh/lang.zsh
+++ .config/zsh/lang.zsh
@@ -6,6 +6,7 @@ export PATH=$PATH:~/.local/bin
 # Rust
 export PATH=$PATH:~/.local/share/cargo/bin
 export CARGO_HOME=~/.local/share/cargo
+export RUSTUP_HOME=~/.local/share/rustup
 
 # Ruby
 export PATH=$PATH:~/.gem/ruby/2.4.0/bin
